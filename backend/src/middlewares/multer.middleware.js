f4f91690a9689e90250e3eeb9b7e883e6b491545
RayyanRS6 <mrayyansalman69@gmail.com>
2026-06-09 20:48:30 +0500

    Add only frontend code to root directory

--- backend/src/middlewares/multer.middleware.js
+++
@@ -1,6 +0,0 @@
-import multer from "multer";
-
-// Use memory storage instead of disk storage for serverless environments
-const storage = multer.memoryStorage();
-
-export const upload = multer({ storage });
